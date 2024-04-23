b96ff85c1302e75d3be7e1220af81ef210f2ab1e
rezowan <rezowan311410@gmail.com>
2024-04-23 12:10:00 +0600

    test

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -315,10 +315,7 @@ const Navbar = () => {
           <div className="md:flex mx-5 lg_res:mx-0">
             <div className="flex">
               <div className="w-3/12 w-44 h-12">
-                <img
-                  className="w-full h-full"
-                  src="../../assets/logoremovebg.png"
-                />
+                <img className="w-full h-full" src="../../assets/profile.png" />
               </div>
               <div className="md:hidden ml-auto" onClick={handleMenu}>
                 <Image
